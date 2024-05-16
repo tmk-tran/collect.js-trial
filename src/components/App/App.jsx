9f7d2fcfecf5014f60286f35d02b064851bf08dd
T Mark <tmk1.tran@gmail.com>
2024-05-16 00:08:50 -0500

    moved package.json, rearranged file order, imported MUI dependencies

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PaymentView from "../PaymentView/PaymentView";
+
+function App() {
+  return (
+    <div className="App">
+      <PaymentView />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PaymentView/PaymentView.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+ 
+export default function PaymentView() {
+    return (
+        <Box>
+            Payment View Yay
+        </Box>
+    );
+}
